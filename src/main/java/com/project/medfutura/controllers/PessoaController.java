f56d0df1dfa190daedc3aa1ad7b590f1d369017e
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-01-23 01:49:00 -0300

    Controller created

---
+++ src/main/java/com/project/medfutura/controllers/PessoaController.java
@@ -0,0 +1,56 @@
+package com.project.medfutura.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.medfutura.entities.Pessoa;
+import com.project.medfutura.dto.PessoaDTO;
+import com.project.medfutura.services.PessoaService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+    
+    @Autowired
+    private PessoaService service;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid Pessoa entity){
+        service.create(entity);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PessoaDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping("/{term}")
+    public ResponseEntity<PessoaDTO> searchByTerm(@PathVariable String term){
+        //TODO: Implement searchByTerm
+        return ResponseEntity.ok(service.searchByTerm(term));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PessoaDTO> update(@PathVariable Long id, @RequestBody @Valid Pessoa entity){
+        return ResponseEntity.ok(service.update(id, entity));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
