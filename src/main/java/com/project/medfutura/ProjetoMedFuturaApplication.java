bd5f3a99cac1d58b99d820efc28dd3c4f02d563e
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-01-23 00:11:56 -0300

    Project Created

---
+++ src/main/java/com/project/medfutura/ProjetoMedFuturaApplication.java
@@ -0,0 +1,13 @@
+package com.project.medfutura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMedFuturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMedFuturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/medfutura/ProjetoMedFuturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.medfutura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMedFuturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
