5f1767b8dd884b9b66f44abe2a67750607c1bb5c
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-01-24 02:17:38 -0300

    feat: add DataIntegrityViolationException as BAD_REQUEST

--- src/main/java/com/project/medfutura/controllers/exception/GlobalException.java
+++ src/main/java/com/project/medfutura/controllers/exception/GlobalException.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.hibernate.ObjectNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -28,5 +29,10 @@ public class GlobalException {
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<String> handleObjectNotFoundException(ObjectNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
